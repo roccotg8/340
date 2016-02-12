5ba4b22b4f5471715731cf6dd5fc0fd74b14d21c
roccotg8 <rtg8@njit.edu>
2016-02-11 19:50:12 -0500

    adding test script to repository
    this would be a longer description of this change set
    i would talk a lot more about shit here
    
    but this is an example and i dont have anything to say

---
+++ test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+thisVariable="splunge"
+
+echo "the name of this variable is $thisVariable"
+echo "the name of this script is $0"
+echo "argument 0 is $0"
+echo "argument 1 is $1"
+echo "argument 2 is $2"
+echo "argument 3 is $3"
+echo "argument 4 is $4"
+echo "argument 5 is $5"
+
+for i in $@
+do
+    if [ $i == "-h" ] ;then 
+        echo "this should be useful... sorry"
+    fi
+    echo "command line option: $i"
+done
